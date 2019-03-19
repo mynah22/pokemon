884b883db5f3cc4a2993a5325ce3a4ce97596abe
Will <422wam@gmail.com>
2019-03-18 20:24:43 -0600

    pokecenter now resets pp

--- heal.py
+++ heal.py
@@ -3,5 +3,8 @@ from clear import clear
 def heal(pl):
     for poke in pl.team:
         poke.hp = int(poke.stats.stats['hp'])
+        for move in poke.moveset:
+        	if poke.moveset[move]:
+	        	poke.moveset[move].remainingUsagePoints = poke.moveset[move].maxUsagePoints
    # playsound('heal.mp3')
     xxx=raw_input('Pokemon healed!')
