e35c31dff3d94bd8267f144b31b345cd6b5f97a5
Will <422wam@gmail.com>
2019-03-18 20:12:16 -0600

    ko/lvl/evl evaluated between attacks

--- hunt.py
+++ hunt.py
@@ -66,11 +66,10 @@ def hunt(pl):
         #user selection = attack
         if inp=='attack':
             clear()  
-            #
             #move select phase
             atkid=atkMenu(pl.team[0])
             rmonAtkID=rmon.wildAttack()
-            #first attacker based on move speed priority and atkstat speed
+            #select first attacker based on move speed priority and atkstat speed
             if pl.team[0].moveset[atkid].speedPriority == rmon.moveset[rmonAtkID].speedPriority:
                 if pl.team[0].stats.atkstats['speed'] > rmon.stats.atkstats['speed']:
                     firstAttacker=pl.team[0]
@@ -82,7 +81,7 @@ def hunt(pl):
                 firstAttacker=pl.team[0]
             elif pl.team[0].moveset[atkid].speedPriority < rmon.moveset[rmonAtkID].speedPriority:
                 firstAttacker=rmon
-            #second attacker and moveid by reference
+            #select second attacker and moveid by reference
             if firstAttacker == pl.team[0]:
                 firstMoveID = atkid
                 secondAttacker = rmon
@@ -91,7 +90,9 @@ def hunt(pl):
                 firstMoveID = rmonAtkID
                 secondAttacker = pl.team[0]
                 secondMoveID = atkid
-            #first atk                        
+            #
+            ####first atk   
+            #                     
             if firstMoveID != 5:
             #if at least 1 move with positive PP
                 print firstAttacker.name+' uses '+firstAttacker.moveset[firstMoveID].name+"!"
@@ -117,43 +118,8 @@ def hunt(pl):
                     print firstAttacker.name+' missed!'  
             wait=raw_input('press enter...')
             ##
+            ########## after atk eval
             ##
-            #second atk
-            ##
-            ##
-            if secondMoveID != 5:
-            #if at least 1 move with positive PP
-                print secondAttacker.name+' uses '+secondAttacker.moveset[secondMoveID].name+"!"
-                secondPreAtkHP=secondAttacker.hp
-                firstPreAtkHP=firstAttacker.hp
-                if secondAttacker.moveset[secondMoveID].cast(secondAttacker, firstAttacker):
-                    print firstAttacker.name +' took '+str(firstPreAtkHP-firstAttacker.hp)+' damage!'
-                    if not secondPreAtkHP == secondAttacker.hp: 
-                        print secondAttacker.name +' took '+str(secondPreAtkHP - secondAttacker.hp)+' damage!'
-                else:
-                    print secondAttacker.name+' missed!'                
-            elif atkid == 5:
-            #if no moves left, struggle
-                secondPreAtkHP=secondAttacker.hp
-                firstPreAtkHP=firstAttacker.hp
-                print secondAttacker.name+' has no moves left!'
-                print secondAttacker.name+' struggles!'
-                if Struggle().cast(secondAttacker, firstAttacker):
-                    print firstAttacker.name +' took '+str(firstPreAtkHP-firstAttacker.hp)+' damage!'
-                    if not secondPreAtkHP == secondAttacker.hp: 
-                        print secondAttacker.name +' took '+str(secondPreAtkHP - secondAttacker.hp)+' damage!'        
-                else:
-                    print secondAttacker.name+' missed!'  
-            wait=raw_input('press enter...')
-            
- 
-
-
-            ##
-            ##
-            ##Attacks over
-            ##
-
             #If enemy monster dies
             if rmon.hp<1:
                 clear()
@@ -190,7 +156,7 @@ def hunt(pl):
                                 pl.team[0].hp=pl.team[0].stats.stats['hp']
                             else:
                                 pl.team[0].stats.calcstat(stat, pl.team[0].level)
-                     
+                        pl.team[0].wildMovesetFill()
                         print pl.team[0].name+' grew to level '+str(pl.team[0].level)+'!'
                     #evolve process
                     if pl.team[0].evolvable:
@@ -218,6 +184,124 @@ def hunt(pl):
                 #break loop
                 encfin=True
             
+            #if player monster is dead
+            if pl.team[0].hp<1:
+                clear()
+                #if player has usable monsters
+                if pl.countawake():
+                    print pl.name+"'s "+pl.team[0].name+' K.O.!'
+                    xxx=raw_input('\npress enter')
+                    pl.team.head(pl.team.index(swapMen(pl)))
+                    print 'keep fighing '+rmon.name+'?'
+                    swapch=pMenu(binMen)
+                    if swapch=='yes':
+                        pass
+                    elif swapch=='no':
+                        print pl.name+' got away safely!'
+                        xxx=raw_input('\npress enter')
+                        encfin=True
+                #blackout if no monsters usable
+                else:
+                    print pl.name+' is out of usable pokemon!'
+                    xxx=raw_input('\npress enter')
+                    encfin=True
+            ##########
+            #second atk
+            ##
+            ##
+            if secondAttacker.hp > 0:
+                if secondMoveID != 5:
+                #if at least 1 move with positive PP
+                    print secondAttacker.name+' uses '+secondAttacker.moveset[secondMoveID].name+"!"
+                    secondPreAtkHP=secondAttacker.hp
+                    firstPreAtkHP=firstAttacker.hp
+                    if secondAttacker.moveset[secondMoveID].cast(secondAttacker, firstAttacker):
+                        print firstAttacker.name +' took '+str(firstPreAtkHP-firstAttacker.hp)+' damage!'
+                        if not secondPreAtkHP == secondAttacker.hp: 
+                            print secondAttacker.name +' took '+str(secondPreAtkHP - secondAttacker.hp)+' damage!'
+                    else:
+                        print secondAttacker.name+' missed!'                
+                elif atkid == 5:
+                #if no moves left, struggle
+                    secondPreAtkHP=secondAttacker.hp
+                    firstPreAtkHP=firstAttacker.hp
+                    print secondAttacker.name+' has no moves left!'
+                    print secondAttacker.name+' struggles!'
+                    if Struggle().cast(secondAttacker, firstAttacker):
+                        print firstAttacker.name +' took '+str(firstPreAtkHP-firstAttacker.hp)+' damage!'
+                        if not secondPreAtkHP == secondAttacker.hp: 
+                            print secondAttacker.name +' took '+str(secondPreAtkHP - secondAttacker.hp)+' damage!'        
+                    else:
+                        print secondAttacker.name+' missed!'  
+                wait=raw_input('press enter...')
+            ##
+            ##Attacks over
+            ##
+            #If enemy monster dies
+            if not encfin:
+                if rmon.hp<1:
+                    clear()
+                    #add evs
+                    for stat in pl.team[0].stats.statNames:
+                        if pl.team[0].stats.ev[stat]<100:
+                            pl.team[0].stats.ev[stat]+=1
+                            if stat=='hp':
+                                pl.team[0].stats.calchp(pl.team[0].level)
+                            else:
+                                pl.team[0].stats.calcstat(stat, pl.team[0].level)
+                        #hold evs at max (100)
+                        else:
+                            pl.team[0].stats.ev[stat]=100
+                    #display
+                    print rmon.name+' K.O.'
+                    #add xp
+                    if pl.team[0].level<100:    
+                        xpg = (rmon.stats.faintxp*rmon.level)/7
+                        pl.team[0].xp+=xpg
+                        print pl.team[0].name+' Gained '+str(xpg)+'xp!'
+                        print str(pl.team[0].xp)+' / '+str(pl.team[0].nextxp) +' ('+str(pl.team[0].nextxp-pl.team[0].xp)+'   remaining) '
+                        #level up process
+                        while pl.team[0].xp >= pl.team[0].nextxp and pl.team[0].level<100:
+                            pl.team[0].level += 1
+                            if pl.team[0].level==100:
+                                pl.team[0].xp=pl.team[0].nextxp
+                                pl.team[0].nextxp=pl.team[0].xp+1
+                            else:
+                                pl.team[0].nextxpcalc()
+                            for stat in pl.team[0].stats.statNames:
+                                if stat == 'hp':
+                                    pl.team[0].stats.calchp(pl.team[0].level)
+                                    pl.team[0].hp=pl.team[0].stats.stats['hp']
+                                else:
+                                    pl.team[0].stats.calcstat(stat, pl.team[0].level)
+                         
+                            print pl.team[0].name+' grew to level '+str(pl.team[0].level)+'!'
+                        #evolve process
+                        if pl.team[0].evolvable:
+                            if pl.team[0].level>=pl.team[0].evlvl:
+                                print pl.team[0].name +' has evolved into '+pokeclasses[pl.team[0].pokedexid]().name+'!'
+                                tmpevmon = pokeclasses[pl.team[0].pokedexid]()
+                                pl.dex.caught(tmpevmon)
+                                tmpevmon.xp = pl.team[0].xp
+                                tmpevmon.level = pl.team[0].level
+                                tmpevmon.nextxpcalc()
+                                tmpevmon.stats.ev = pl.team[0].stats.ev
+                                tmpevmon.stats.iv = pl.team[0].stats.iv
+                                for stat in tmpevmon.stats.statNames:
+                                    if stat == 'hp':
+                                        tmpevmon.stats.calchp(tmpevmon.level)
+                                        tmpevmon.hp=tmpevmon.stats.stats['hp']
+                                    else:
+                                        tmpevmon.stats.calcstat(stat, tmpevmon.level)
+                                pl.team[0]=tmpevmon
+                    #$$$$$ boieeee
+                    reward=rrang(1000)
+                    pl.money+=reward
+                    print pl.name+' found $'+str(reward)+'!\n'
+                    raw_input('press return')
+                    #break loop
+                    encfin=True
+                
             #if player monster is dead
             if pl.team[0].hp<1:
                 clear()
